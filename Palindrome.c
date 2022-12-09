c895676cce18f1c2ae6971ae36a1d1a11ed02238
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2022-12-09 11:48:16 +0530

    #include<stdio.h> int main() {     int n,rem,sum=0,q;     scanf("%d",&n);     q=n;     while(q!=0)     {         rem=q%10;         sum=sum*10+rem;         q=q/10;     }     if(n==sum)     {         printf("True");     }     else     {         printf("False");     } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,r,s=0;
+    int n,rem,sum=0,q;
     scanf("%d",&n);
-    for(i=n;i>0; )
+    q=n;
+    while(q!=0)
     {
-        r=i%10;
-        s=s*10+r;
-        i=i/10;
+        rem=q%10;
+        sum=sum*10+rem;
+        q=q/10;
     }
-    if(s==n)
+    if(n==sum)
     {
         printf("True");
     }
