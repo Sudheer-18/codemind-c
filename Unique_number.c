9a8f332652fa680bc8c2f0a2782399eac2d239bf
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-05-26 11:36:46 +0530

    #include <stdio.h>
    int isUniqueNumber(int number) {
        int digits[10] = {0};  // Array to store the count of each digit
        int digit;
    
        while (number != 0) {
            digit = number % 10;  // Get the last digit
            digits[digit]++;     // Increment the count for the digit
            number /= 10;        // Remove the last digit
        }
    
        for (int i = 0; i < 10; i++) {
            if (digits[i] > 1) {
                return 0;  // Number is not unique
            }
        }
    
        return 1;  // Number is unique
    }
    
    int main() {
        int number;
    
        scanf("%d", &number);
    
        if (isUniqueNumber(number)) {
            printf("Unique Number");
        } else {
            printf("Not Unique Number");
        }
    
        return 0;
    }

--- Unique_number.c
+++ Unique_number.c
@@ -1,44 +1,34 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-	int n,q,m,l;
-	scanf("%d",&n);
-	m=log10(n)+1;
-	q=n;
-	int arr[m],i,j,cnt=0;
-	i=0;
-	while(q!=0)
-	{
-		//i=0;
-		int r;
-		r=q%10;
-		arr[i]=r;
-		q=q/10;
-		i++;
-		//printf("%d",arr[i]);
-	}
-		
-	for(i=0;i<m;i++)
-	{
-		for(j=0;j<m;j++)
-		{
-			if(i!=j)
-			{
-				if(arr[i]==arr[j])
-				{
-					cnt++;
-				}
-			}
-		}
-	}
-	if(cnt==0)
-	{
-		printf("Unique Number");
-	}
-	else
-	{
-		printf("Not Unique Number");
-	}
-	
+#include <stdio.h>
+
+int isUniqueNumber(int number) {
+    int digits[10] = {0};  // Array to store the count of each digit
+    int digit;
+
+    while (number != 0) {
+        digit = number % 10;  // Get the last digit
+        digits[digit]++;     // Increment the count for the digit
+        number /= 10;        // Remove the last digit
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (digits[i] > 1) {
+            return 0;  // Number is not unique
+        }
+    }
+
+    return 1;  // Number is unique
+}
+
+int main() {
+    int number;
+
+    scanf("%d", &number);
+
+    if (isUniqueNumber(number)) {
+        printf("Unique Number");
+    } else {
+        printf("Not Unique Number");
+    }
+
+    return 0;
 }
