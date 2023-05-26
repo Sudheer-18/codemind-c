941d16b95ea1b843b52fd880753adf020fd651fe
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-05-26 11:52:14 +0530

    #include <stdio.h>
    int factorial(int n) {
        if (n == 0 || n == 1)
            return 1;
        else
            return n * factorial(n - 1);
    }
    
    int isStrongNumber(int number) {
        int originalNumber = number;
        int sum = 0;
    
        while (number != 0) {
            int digit = number % 10;
            sum += factorial(digit);
            number /= 10;
        }
    
        if (sum == originalNumber)
            return 1; // Strong number
        else
            return 0; // Not a strong number
    }
    
    int main() {
        int number;
        scanf("%d", &number);
    
        if (isStrongNumber(number))
            printf("The number %d is a strong number
    ", number);
        else
            printf("The number %d is not a strong number
    ", number);
    
        return 0;
    }

--- Strong_Number.c
+++ Strong_Number.c
@@ -1,29 +1,38 @@
-#include<stdio.h>
-int main()
-{
-    int n,sum=0,num,q,i,p;
-    scanf("%d",&n);
-    q=n;
-    while(n)
-    {
-        i=1;
-        p=1;
-        num=n%10;
-        while(i<=num)
-        {
-            p=p*i;
-            i++;
-        }
-        sum=sum+p;
-        n=n/10;
-    }
-    if(sum==q)
-    {
-        printf("The number %d is a strong number",q);
-    }
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
     else
-    {
-        printf("The number %d is not a strong number",q);
+        return n * factorial(n - 1);
+}
+
+int isStrongNumber(int number) {
+    int originalNumber = number;
+    int sum = 0;
+
+    while (number != 0) {
+        int digit = number % 10;
+        sum += factorial(digit);
+        number /= 10;
     }
 
+    if (sum == originalNumber)
+        return 1; // Strong number
+    else
+        return 0; // Not a strong number
+}
+
+int main() {
+    int number;
+    scanf("%d", &number);
+
+    if (isStrongNumber(number))
+        printf("The number %d is a strong number
+", number);
+    else
+        printf("The number %d is not a strong number
+", number);
+
+    return 0;
 }
